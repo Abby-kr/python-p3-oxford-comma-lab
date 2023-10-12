5cb149af3b370f30e02e74ead5d637c57895730d
Abby-kr <abby.a.otieno@gmail.com>
2023-10-12 16:44:51 +0300

    Completed Lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,43 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return items[0] + " and " + items[1]
+    elif len(items) >= 3:
+        last_item = items[-1]
+        #print(last_item)
+    
+        i = 0
+        remaining_items = list()
+        while i < len(items) - 1:
+            remaining_items.append(items[i])
+            i += 1
+        #print(remaining_items)
+        new_string = ", ".join(remaining_items)
+        #print(new_string)
+        final_string = new_string + ', and ' + last_item
+        return final_string
+        
+
+
+
+
+
+
+
+
+
+
+
+    #removed_elements = list()
+    #last_element = items[-1]
+
+    #for element in range(len(items) - 1):
+        #removed_elements.append(element)
+
+
+    #def join_all_elements(string1,string2):
+        #first_string = ",".join(string1)
+        #final_string = first_string + 'and' + string2
+        #return final_string
+    #join_all_elements(removed_elements, last_element)
